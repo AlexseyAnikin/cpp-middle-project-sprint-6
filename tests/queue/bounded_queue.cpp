2ec0cc8d55b0cbdae0c830907a71ce8258abe061
Ivan Losev <ivan.s.nn97@gmail.com>
2025-10-10 10:27:20 +0200

    Improve container and description

--- tests/queue/bounded_queue.cpp
+++ tests/queue/bounded_queue.cpp
@@ -2,4 +2,6 @@
 
 #include "queue/bounded_queue.hpp"
 
+TEST(BasicCheck, SimpleCheck) { EXPECT_EQ(1 + 1, 2); }
+
 // здесь ваш код
--- tests/task_dispatcher.cpp
+++ tests/task_dispatcher.cpp
@@ -2,4 +2,6 @@
 
 #include "task_dispatcher.hpp"
 
+TEST(BasicCheck, SimpleCheck) { EXPECT_EQ(1 + 1, 2); }
+
 // здесь ваш код
