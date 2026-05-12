94c1bfc99ef703d30fc2587f4e3b7b9cb9e5150a
Anikin Alex <Anikin.AS2@spacecorp.ru>
2026-05-12 21:22:45 +0300

    feat: реализовал thread pool

--- src/thread_pool/thread_pool.cpp
+++ src/thread_pool/thread_pool.cpp
@@ -2,6 +2,52 @@
 
 namespace dispatcher::thread_pool {
 
-// здесь ваш код
+ThreadPool::ThreadPool(std::shared_ptr<queue::PriorityQueue> queue, std::size_t thread_count) :
+                        queue_(std::move(queue))
+{
+    if(!queue_)
+    {
+        throw std::invalid_argument("ThreadPool requires queue");
+    }
+
+    if(thread_count == 0)
+    {
+        thread_count = 1;
+    }
+
+    workers_.reserve(thread_count);
+
+    for(std::size_t i = 0; i < thread_count; ++i)
+    {
+        workers_.emplace_back(&ThreadPool::Worker, this);
+    }
+}
+
+void ThreadPool::Worker()
+{
+    while(true)
+    {
+        auto task = queue_->pop();
+
+        if(!task.has_value())
+        {
+            return;
+        }
+
+        try
+        {
+            (*task)();
+        } 
+        catch (...)
+        {
+            //Исключение из задачи не должно завершать worker потоков    
+        }
+    }
+}
+
+ThreadPool::~ThreadPool()
+{
+    queue_->shutdown();
+}
 
 } // namespace dispatcher::thread_pool
