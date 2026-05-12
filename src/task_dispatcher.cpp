28c6cb496265cf6df6598b4b034bea715b3f32c2
Anikin Alex <Anikin.AS2@spacecorp.ru>
2026-05-12 21:34:34 +0300

    feat: реализовал task dispatcher

--- src/task_dispatcher.cpp
+++ src/task_dispatcher.cpp
@@ -1,7 +1,40 @@
 #include "task_dispatcher.hpp"
 
+#include <utility>
 namespace dispatcher {
 
-// здесь ваш код
+std::map<TaskPriority, queue::QueueOptions>
+TaskDispatcher::DefaultQueueOptions() {
+    return {
+        {
+            TaskPriority::High,
+            queue::QueueOptions{
+                .bounded = true,
+                .capacity = 1000,
+            },
+        },
+        {
+            TaskPriority::Normal,
+            queue::QueueOptions{
+                .bounded = false,
+                .capacity = std::nullopt,
+            },
+        },
+    };
+}
+
+TaskDispatcher::TaskDispatcher(
+    std::size_t thread_count,
+    std::map<TaskPriority, queue::QueueOptions> queue_options)
+    : queue_(std::make_shared<queue::PriorityQueue>(std::move(queue_options))),
+      thread_pool_(queue_, thread_count) {}
+
+void TaskDispatcher::schedule(TaskPriority priority, std::function<void()> task)
+{
+    queue_->push(priority, std::move(task));
+}
+
+TaskDispatcher::~TaskDispatcher() = default;
+}
 
 } // namespace dispatcher
