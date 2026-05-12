23a381c135f80e92acd3488c3b49d4d32863c62e
Anikin Alex <Anikin.AS2@spacecorp.ru>
2026-05-12 20:26:28 +0300

    feat: реализовал Unbounded queue

--- src/queue/unbounded_queue.cpp
+++ src/queue/unbounded_queue.cpp
@@ -7,6 +7,31 @@
 
 namespace dispatcher::queue {
 
-// здесь ваш код
+UnboundedQueue::UnboundedQueue(int /*capacity*/){}
+
+voif UnboundedQueue::push(std::function<void()> task)
+{
+    std::lock_guard lock(mutex_);
+
+    tasks_.push(std::move(task));
+}
+
+std::optional<std::function<void()>> UnboundedQueue::try_pop()
+{
+    std::lock_guard lock(mutex_);
+
+    if(tasks_.empty())
+    {
+        return std::nullopt;
+    }
+
+    auto task = std::move(tasks_.front());
+
+    tasks_.pop();
+
+    return task;
+}
+
+UnboundedQueue::~UnboundedQueue() = default;
 
 } // namespace dispatcher::queue
